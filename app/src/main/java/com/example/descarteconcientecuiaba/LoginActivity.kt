757fb5fa4e9232afd79ab43510ad179e7f66d1b8
Roberto Frisanco dos Santos <roberto.frisanco@univag.iemat>
2024-10-18 14:05:14 -0400

    add 2 activity and navigation between

---
+++ app/src/main/java/com/example/descarteconcientecuiaba/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.example.descarteconcientecuiaba
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var buttonAbrir: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        buttonAbrir = findViewById(R.id.button_login)
+        buttonAbrir.setOnClickListener{
+
+            val intent = Intent(
+                this,
+                MainActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+    }
+}
